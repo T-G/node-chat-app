3212489078d3f36a7be0a8c66e87f0ae4b501db4
T-G <pronoygomes@yahoo.com>
2017-07-06 20:18:19 +0100

    format createdAt using momentjs

--- playground/time.js
+++ playground/time.js
@@ -1,6 +1,12 @@
+// var moment = require("moment");
+
+// // Jan 1st 1970 00:00:00 am
+
+// var date = moment();
+// // console.log(date.format("MMM Do YYYY"));
+
 var moment = require("moment");
-
-// Jan 1st 1970 00:00:00 am
-
 var date = moment();
-console.log(date.format());
+
+console.log(date.format("hh[:]mm a"));
+console.log(date.format("h[:]mm a"));
--- public/js/index.js
+++ public/js/index.js
@@ -11,17 +11,19 @@ socket.on("disconnect", function(){
 });
 
 socket.on("newMessage", function(message){
-    console.log("New message received. ", message);
+    //console.log("New message received. ", message);
+    var formattedTime = moment(message.createdAt).format("h:mm a");
     var li = jQuery("<li></li>");
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     jQuery("#messages").append(li);
 });
 
 socket.on("newLocationMessage", function(message){
+    var formattedLocationTime  = moment(message.createdAt).format("h:mm a"); 
     var li = jQuery("<li></li>");
     var a = jQuery("<a target='_blank'>My current location</a>");
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from}: ${formattedLocationTime} `);
     a.attr("href", message.url);
     li.append(a);
     jQuery("#messages").append(li);
